8878171e0936708c2ca8119f46fb90fd4842f827
Thomas Zhao <Thomasjzhao@gmail.com>
2023-10-25 11:50:45 -0400

    First addition

---
+++ version_control.py
@@ -0,0 +1,37 @@
+#Thomas Zhao
+def menu():
+  print('Menu')
+  print(8*'-')
+  print("1. Encode")
+  print("2. Decode")
+  print("3. Quit")
+  print()
+def encode(password):
+  encode_password = ''
+  x = 0
+  for a in range(len(password)):
+    x = int(password[a])+3
+    if x>9:
+      x = x%10
+    encode_password += str(x)
+  return encode_password
+if __name__ == "__main__":
+    menu()
+    run = True
+    encoded_password= ''
+    orig_password = ''
+    while run:
+      choice = input("Please enter an option: ")
+      if choice =="1":
+          password = input("Please enter your password to encode: ")
+          encode(password)
+          print("Your password has been encoded and stored!")
+          print()
+          pass
+      if choice=='2':
+          print("The encoded password is",encoded_password, end =' ')
+          print("and the original password is",orig_password)
+          print()
+          pass
+      if choice=='3':
+          run = False
